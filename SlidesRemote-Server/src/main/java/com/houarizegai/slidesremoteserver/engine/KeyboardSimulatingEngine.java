adcca07841afc679c5b4497eb22925e189e5c215
HouariZegai <houarizegai14@gmail.com>
2020-05-31 17:04:39 +0100

    Server - make keyboard simulating engine

---
+++ SlidesRemote-Server/src/main/java/com/houarizegai/slidesremoteserver/engine/KeyboardSimulatingEngine.java
@@ -0,0 +1,23 @@
+package com.houarizegai.slidesremoteserver.engine;
+
+import java.awt.*;
+import java.awt.event.KeyEvent;
+
+public class KeyboardSimulatingEngine {
+
+    private Robot robot;
+
+    public KeyboardSimulatingEngine() {
+        try {
+            robot = new Robot();
+        } catch(AWTException e) {
+            e.printStackTrace();
+        }
+    }
+
+
+    public void pressKey(int key) { // Simulate a key press
+        robot.keyPress(key);
+        robot.keyRelease(key);
+    }
+}
