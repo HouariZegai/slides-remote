60dce719e7766e4d254c7ce787687b93fdfc8f19
HouariZegai <houarizegai14@gmail.com>
2020-06-03 00:39:47 +0100

    Added the IP addresses to UI with listeners (controller)

--- SlidesRemote-Server/src/main/java/com/houarizegai/slidesremoteserver/controllers/MainController.java
+++ SlidesRemote-Server/src/main/java/com/houarizegai/slidesremoteserver/controllers/MainController.java
@@ -1,10 +1,12 @@
 package com.houarizegai.slidesremoteserver.controllers;
 
+import com.houarizegai.slidesremoteserver.App;
 import com.houarizegai.slidesremoteserver.engine.QRCodeEngine;
 import com.houarizegai.slidesremoteserver.engine.SocketServer;
 import com.houarizegai.slidesremoteserver.utils.NetworkUtils;
 import com.houarizegai.slidesremoteserver.utils.RegexChecker;
-import com.jfoenix.controls.JFXTextField;
+import com.jfoenix.controls.JFXComboBox;
+import javafx.application.Platform;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Label;
@@ -12,12 +14,17 @@ import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 
 import java.net.URL;
+import java.util.List;
+import java.util.Map;
 import java.util.ResourceBundle;
 
 public class MainController implements Initializable {
 
     @FXML
-    private JFXTextField fieldIpAddress;
+    private JFXComboBox<String> comboNetworkName;
+
+    @FXML
+    private Label lblIpAddress;
 
     @FXML
     private ImageView imgQRCode;
@@ -27,24 +34,36 @@ public class MainController implements Initializable {
 
     private SocketServer socketServer;
 
+    // Available network addresses in my PC
+    private static Map<String, String> networkAddresses;
+
     @Override
     public void initialize(URL location, ResourceBundle resources) {
+        comboNetworkName.valueProperty().addListener(e -> {
+            updateIpLbl();
+            changeQRCodeImg();
+        });
         onRefresh();
-
-        fieldIpAddress.setOnKeyReleased(e -> changeQRCodeImg());
     }
 
     @FXML
     private void onRefresh() {
-        String myIp = NetworkUtils.getMyIPAddress();
-        fieldIpAddress.setText(myIp);
-        changeQRCodeImg();
+        comboNetworkName.getItems().clear();
+        networkAddresses = NetworkUtils.getMyIPv4Addresses();
+
+        if(!networkAddresses.isEmpty()) {
+
+            for(String key: networkAddresses.keySet()) {
+                comboNetworkName.getItems().add(key);
+            }
+            changeQRCodeImg();
+        }
     }
 
     @FXML
     private void onStart() {
         socketServer = new SocketServer();
-        if(RegexChecker.isIP(fieldIpAddress.getText())) {
+        if(RegexChecker.isIP(lblIpAddress.getText())) {
             socketServer.start();
             lblStatus.setText("Connected");
         } else {
@@ -58,9 +77,18 @@ public class MainController implements Initializable {
         lblStatus.setText("Disconnected");
     }
 
+    @FXML
+    private void onClose() {
+        Platform.exit();
+    }
+
+    @FXML
+    private void onHide() {
+        App.stage.setIconified(true);
+    }
 
     private void changeQRCodeImg() {
-        String myIP = fieldIpAddress.getText();
+        String myIP = lblIpAddress.getText();
         if(RegexChecker.isIP(myIP)) {
             Image generatedQRCode = QRCodeEngine.encode(myIP, 250, 250);
             if(generatedQRCode != null) {
@@ -71,6 +99,11 @@ public class MainController implements Initializable {
             }
         }
 
+    }
+
+    private void updateIpLbl() {
+        String selectedNetworkName = networkAddresses.get(comboNetworkName.getSelectionModel().getSelectedItem());
+        lblIpAddress.setText(selectedNetworkName);
     }
 
 }
